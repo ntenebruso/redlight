6ec06dac946f98ce156100d0709b05c230c2cdc0
Nick Tenebruso <nickten2006@gmail.com>
2022-08-20 18:51:56 -0400

    Reply section bug fixes

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -6,21 +6,34 @@ export default function Comment({ comment }) {
     return (
         <div className="flex mt-9">
             <div className="mr-4 w-12 flex flex-col items-center text-center">
-                <span className="bg-neutral-600 inline-block p-2 w-full rounded-md mb-4">
-                    {new Intl.NumberFormat("en-US", {
-                        notation: "compact",
-                        compactDisplay: "short",
-                    }).format(comment.score)}
-                </span>
-                <div
-                    className="h-full w-1 bg-neutral-600 hover:cursor-pointer hover:bg-white"
-                    onClick={() => setThreadHidden(!threadHidden)}
-                />
+                {threadHidden ? (
+                    <button
+                        className="btn w-7 h-7 leading-none"
+                        onClick={() => setThreadHidden(false)}
+                    >
+                        +
+                    </button>
+                ) : (
+                    <>
+                        <span className="bg-neutral-600 inline-block p-2 w-full rounded-md mb-4">
+                            {new Intl.NumberFormat("en-US", {
+                                notation: "compact",
+                                compactDisplay: "short",
+                            }).format(comment.score)}
+                        </span>
+                        <div
+                            className="h-full w-1 bg-neutral-600 hover:cursor-pointer hover:bg-white"
+                            onClick={() => setThreadHidden(true)}
+                        />
+                    </>
+                )}
             </div>
             <div className="flex-1">
                 <p className="font-bold">u/{comment.author}</p>
                 <div
-                    className="post-body mt-2"
+                    className={`post-body mt-2 ${
+                        threadHidden ? "hidden" : "block"
+                    }`}
                     dangerouslySetInnerHTML={{
                         __html: comment.body_html,
                     }}
